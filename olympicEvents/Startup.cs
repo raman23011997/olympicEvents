20b01e0136c5f46822d68209d1223ab9515b45c3
raman23011997 <sramanjeet57@gmail.com>
2017-10-03 22:34:35 -0400

    Add project files.

---
+++ olympicEvents/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(olympicEvents.Startup))]
+namespace olympicEvents
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
